fc059b45623c080be0e634a9696b01f1a5f61d26
Renjie Wu <renjiewu@mit.edu>
2024-09-19 11:50:12 -0400

    /new route

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3030;
 
 let users = [];
+let sortedUsers = [];
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
@@ -16,14 +17,26 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/new', (req, res) => {
+    res.json(sortedUsers);
+});
+
 app.post('/input', (req, res) => {
     const name = escape(req.body.name);
-    const reactionTime = escape(req.body.reactionTime);
-    const penalty = escape(req.body.penalty);
+    const reactionTime = parseFloat((escape(req.body.reactionTime) / 1000).toFixed(3))
+    const penalty = parseInt(escape(req.body.penalty));
 
     users.push({ name: name, reactionTime: reactionTime, penalty: penalty });
 
-    res.json(users);
+    sortedUsers = users
+        .map(user => ({
+            ...user,
+            totalTime: (user.reactionTime + user.penalty).toFixed(3)
+        }))
+        .sort((a, b) => a.totalTime - b.totalTime)
+        .slice(0, 5);
+
+    res.json(sortedUsers);
 });
 
 app.listen(PORT, () => {
